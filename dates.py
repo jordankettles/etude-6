4e59efe03f7b7196835f1536a107c01f8f54305b
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-03-10 13:42:56 +1300

    create dates.py

---
+++ dates.py
@@ -0,0 +1,27 @@
+import sys
+
+#Author: Jordan Kettles 2147684
+# Date: 10/03/21
+print("Hello World.")
+
+#Read a date and determine whether it is a valid date
+# between the years 1753 and 3000.
+
+#Either state why the date is invalid.
+# or outptu a valid date in the format
+# <dd> <first three char of month> <yyyy>
+# e.g. 02 Apr 1997
+#month input could be the first three letters of the month
+#name all in the same case, all with the first letter upper-case.
+#seperators could be space, / or -
+
+#calcutate whether the date is valid (29th of Feb)
+#How do i calculate leap years?
+#if the year is two numbers, it lies between 1950 and 2049.
+#if >49 +1900 else +2000
+
+#check if the first letter is uppercase:
+#all follwing letters have to be the same case.
+
+#if the first letter is lowercase:
+#all following letters have to be lowercase.
